b3fc6c2331e5f49c72cfc474cdf0be085f1cba05
lucasj <lucasj@MacBook-Pro.local>
2022-12-19 13:03:28 +0800

    init

---
+++ Source/OpenCVNewPlugin/OpenCVNewPlugin.Build.cs
@@ -0,0 +1,69 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class OpenCVNewPlugin : ModuleRules
+{
+
+	private string ThirdPartyPath => Path.GetFullPath(Path.Combine(ModuleDirectory, "../../ThirdParty"));
+
+	public OpenCVNewPlugin(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+		
+		PublicIncludePaths.AddRange(
+			new string[] {
+				// ... add public include paths required here ...
+			}
+			);
+				
+		
+		PrivateIncludePaths.AddRange(
+			new string[] {
+				// ... add other private include paths required here ...
+			}
+			);
+			
+		
+		PublicDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"Core",
+				"OpenCVNewPluginLibrary",
+				"Projects"
+				// ... add other public dependencies that you statically link with here ...
+			}
+			);
+			
+		
+		PrivateDependencyModuleNames.AddRange(
+			new string[]
+			{
+				// ... add private dependencies that you statically link with here ...	
+			}
+			);
+		
+
+		// Add third-party include && lib path
+		var opencvRelativePath = ThirdPartyPath + "/opencv4.6.0/";
+		// var archSuffix = Target.Platform == UnrealTargetPlatform.Win64 ? "x64" : "x86";
+		var platformSuffix = Target.Platform.ToString(); // Win64, Mac, IOS, Android
+		var opencvLibPath = Path.Combine(opencvRelativePath, "lib", platformSuffix);
+		
+		PublicIncludePaths.Add(Path.Combine(opencvRelativePath, "include/opencv4"));
+		PublicAdditionalLibraries.Add(Path.Combine(opencvLibPath, "libopencv_core.a"));
+		PublicAdditionalLibraries.Add(Path.Combine(opencvLibPath, "libopencv_video.a"));
+		PublicAdditionalLibraries.Add(Path.Combine(opencvLibPath, "libopencv_videoio.a"));
+		PublicAdditionalLibraries.Add(Path.Combine(opencvLibPath, "libopencv_imgcodecs.a"));
+		PublicAdditionalLibraries.Add(Path.Combine(opencvLibPath, "libopencv_imgproc.a"));
+		PublicAdditionalLibraries.Add(Path.Combine(opencvLibPath, "libopencv_highgui.a"));
+		PublicAdditionalLibraries.Add(Path.Combine(opencvLibPath, "opencv4/3rdparty/liblibjpeg-turbo.a"));
+		PublicAdditionalLibraries.Add(Path.Combine(opencvLibPath, "opencv4/3rdparty/liblibpng.a"));
+		PublicAdditionalLibraries.Add(Path.Combine(opencvLibPath, "opencv4/3rdparty/liblibtiff.a"));
+		PublicAdditionalLibraries.Add(Path.Combine(opencvLibPath, "opencv4/3rdparty/liblibwebp.a"));
+		PublicAdditionalLibraries.Add(Path.Combine(opencvLibPath, "opencv4/3rdparty/liblibopenjp2.a"));
+		PublicAdditionalLibraries.Add(Path.Combine(opencvLibPath, "opencv4/3rdparty/libittnotify.a"));
+		PublicAdditionalLibraries.Add(Path.Combine(opencvLibPath, "opencv4/3rdparty/libade.a"));
+		
+	}
+}
---
+++ readme.md
@@ -0,0 +1,27 @@
+# OpenCVNewPlugin
+
+
+Unlike UE5 official beta opencv plugin version, this version provides `videoio, ffmpeg` support as well.
+
+Also it can runs with OpenCL with GPU acceleratation.
+
+The purpose of this plugin is to:
+
+- Let users can call opencv io inside your customized plugin;
+- Let you can run camera capture, CV AI model inference etc inside Unreal Engine.
+
+
+## System Support
+
+Currently, it supported:
+
+- [x] macOS (x86 or arm);
+- [x] Windows;
+- [ ] Linux;
+- [ ] iOS;
+- [ ] Android;
+
+
+## Notes
+
+If you find a bug, please create an issue to report it.
